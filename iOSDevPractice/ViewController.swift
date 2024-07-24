e561b903360e1e03438881067a8ce1aecf13c9e6
TakayaShirai <s78taka03@gmail.com>
2024-07-24 17:17:27 +0900

    Initial Commit

---
+++ iOSDevPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSDevPractice
+//
+//  Created by Shirai Takaya on 2024/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
